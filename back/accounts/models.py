fd455f66ceebda380c97c9d8ae5476e1328d0097
siakim.1994@gmail.com <siakim.1994@gmail.com>
2024-11-20 00:30:53 +0900

    Feat : 영화 상세 페이지 api 완성

--- back/accounts/models.py
+++ back/accounts/models.py
@@ -5,6 +5,8 @@ class User(AbstractUser):
     followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
     profile_img = models.ImageField(upload_to='profile-image/',
                                     default='profile-default.png')
+    def __str__(self):
+        return self.username
 
 '''
 프로필 이미지의 경로는 아래와 같이 프론트에 전달
--- back/movies/serializers.py
+++ back/movies/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import Avg, Count
 from rest_framework import serializers
 from .models import Movie, Genre, Country, Review, Director, Actor, Provider, Video
 
@@ -37,6 +38,12 @@ class VideoSerializer(serializers.ModelSerializer):
         model = Video
         exclude = ('movie',)
 
+class ReviewSerializer(serializers.ModelSerializer):
+    likes_count = serializers.IntegerField(source='likes.count', read_only=True)
+    class Meta:
+        model = Review
+        fields = ('id', 'user', 'content', 'rating', 'likes_count', 'is_spoiler', 'created_at')
+
 # 영화 상세 페이지 
 class MovieDetailSerializer(serializers.ModelSerializer):
     
@@ -46,7 +53,27 @@ class MovieDetailSerializer(serializers.ModelSerializer):
     providers = ProviderSerializer(read_only=True, many=True)
     countries = CountrySerializer(read_only=True, many=True)
     videos = VideoSerializer(source='video_set', many=True, read_only=True)
+    average_rating = serializers.SerializerMethodField()
+    top_reviews = serializers.SerializerMethodField()
+    likes_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Movie
-        exclude = ('popularity',)
+        exclude = ('popularity', 'likes') 
+    
+    # 해당 영화에 달린 리뷰들의 평균 rating 계산
+    def get_average_rating(self, obj):
+        avg_rating = obj.review_set.aggregate(average=Avg('rating'))['average']
+        return round(avg_rating, 2) if avg_rating is not None else 0.0
+    
+    # 추천 및 등록일 순으로 상위 5개의 review 가져오기
+    # 리뷰가 없는 경우 빈 리스트 [] 반환 
+    def get_top_reviews(self, obj):
+        top_reviews = obj.review_set.annotate(
+            likes_count=Count('likes')
+        ).order_by('-likes_count', '-created_at')[:5]
+        return ReviewSerializer(top_reviews, many=True).data
+    
+    # 해당 영화를 추천한 유저 수 반환
+    def get_likes_count(self, obj):
+        return obj.likes.count()
