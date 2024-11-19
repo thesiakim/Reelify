34f0ca5b29fc35cdd7ef1da7eb1e4217207447f4
siakim.1994@gmail.com <siakim.1994@gmail.com>
2024-11-19 22:08:28 +0900

    Feat : 회원가입 완성

--- front/src/stores/accounts.js
+++ front/src/stores/accounts.js
@@ -8,44 +8,6 @@ export const useAccountStore = defineStore("account", () => {
 
   // 회원 관련 ===============================================================================================
 
-  // 회원 가입
-  const errorMessage = ref(null);
-  const signUp = function (payload) {
-    const { username, password1, password2 } = payload;
-
-    errorMessage.value = null;
-    axios({
-      method: "post",
-      url: `${API_URL}/accounts/signup/`,
-      data: {
-        username,
-        password1,
-        password2,
-      },
-    })
-      .then((res) => {
-        const password = password1;
-        logIn({ username, password }); // 회원가입 성공 시 자동 로그인
-      })
-      .catch((err) => {
-        if (err.response && err.response.data) {
-          if (err.response.data.username) {
-            errorMessage.value = "이미 존재하는 사용자 이름입니다.";
-          } else if (err.response.data.non_field_errors) {
-            errorMessage.value = "비밀번호가 일치하지 않습니다.";
-          } else if (err.response.data.password1) {
-            errorMessage.value = "비밀번호는 최소 6글자 이상이어야 합니다.";
-          } else {
-            errorMessage.value = "알 수 없는 오류가 발생했습니다.";
-          }
-        } else {
-          errorMessage.value =
-            "서버와의 연결에 문제가 발생했습니다. 다시 시도해주세요.";
-        }
-        console.log(err);
-      });
-  };
-
   // 로그인
   const logIn = function (payload) {
     const { username, password } = payload
@@ -75,5 +37,6 @@ export const useAccountStore = defineStore("account", () => {
     return `${TMDB_URL}${path}`;
   };
 
-  return { signUp, errorMessage, logIn, token, API_URL, getPosterPath, searchResults  };
+  // return { signUp, errorMessage, logIn, token, API_URL, getPosterPath, searchResults  };
+  return { logIn, token, API_URL, getPosterPath, searchResults  };
 });
